72e52bbc059862d9edfc45969ef20f8f1c0de3a6
michal666-bit <gajewski.michal88@gmail.com>
2020-01-26 17:10:04 +0100

    PIII-Kolos2

---
+++ Bank.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Michał_Gajewski_Kolos2
+{
+    class Bank
+    {
+        public event EventHandler OtrzymanoZgloszenie;
+    }
+}
---
+++ ExtensionMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Michał_Gajewski_Kolos2
+{
+    class ExtensionMethod
+    {
+        static void main()
+        {
+
+            string nazwisko = "Kowalski";
+            var sb = new StringBuilder(nazwisko);
+            for (int i = 1; i < nazwisko.Length; i++) sb[i] = '*';
+            Console.WriteLine(sb.ToString());
+
+        }
+
+
+    }
+}
